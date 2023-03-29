83e0c13b33366cae61504e3c425eb2670823b79a
Sevirakath <tomstef34@gmail.com>
2023-03-29 15:47:55 +0200

    Attaque sequence

---
+++ Assets/PlayerCombat.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+    void Update()
+    {
+        if (Imput.Getkeydown(Keycode.Space))
+        {
+        Attack();
+    }
+void Attack()
+{
+    
+}
+}
