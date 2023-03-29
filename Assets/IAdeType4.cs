81b3030a8f73610a7dee42fac9cea6e05e88c6e8
Sevirakath <tomstef34@gmail.com>
2023-03-29 14:52:28 +0200

    Shoot Capsule

---
+++ Assets/IAdeType4.cs
@@ -0,0 +1,91 @@
+//Ne fonctionne pas encore appeler les fonctions est compliquer
+//Melange les 2 scripts
+
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAdeType4 : MonoBehaviour
+{
+    float speeda = 5f;
+    float distancea = 2f;
+    private bool movingRight = true;
+    public Transform groundDetection;
+    public Transform target;
+    public float speedb = 5f;
+    float distancemax = 10;
+    // Start is called before the first frame update
+
+    bool atckType1 = true;
+    void Start()
+    {
+        
+    }
+    private void function1()
+    {
+        transform.Translate(Vector2.right * speeda * Time.deltaTime);
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distancea);
+        if (groundInfo.collider == false)
+        {
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
+    }
+
+
+    private void function2()
+    {
+
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speedb * Time.deltaTime);
+        if (transform.position.y == 3)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
+    }
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, target.transform.position) < distancemax)
+        
+        {
+            atckType1 = false;
+            //if(Input.GetKeyDown(KeyCode.P))
+        }
+        else
+        {
+            atckType1 = true; 
+            if (atckType1)
+            {
+                function1();
+            }
+            else
+            {
+                function2();
+            }
+        }
+        
+       
+    }
+    
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+  
+}
--- Assets/Shoot.cs
+++ Assets/Shoot.cs
@@ -1,34 +1,44 @@
-using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Security;
+using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
+    [SerializeField] private Transform shootingPoint; // Mettre sur Unity là ou le projectile va spawn 
+    [SerializeField] private GameObject bulletPrefab; // Mettre sur Unity l'objet qu'on va faire spawn(donc notre projectile)
+    [SerializeField] private float speed = 5; // Vitesse du projectile
 
-    public Rigidbody projectile;
+    private SpriteRenderer sr; //SpriteRenderer du GameObject  
 
-    public float speed = 20;
-
-
-
-    // Use this for initialization
+    // Start is called before the first frame update
     void Start()
     {
-
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetButtonDown("fire1"))
+        if (Input.GetButtonDown("Fire1")) // Si on appuie sur le bouton ("Fire1") 
         {
-            Rigidbody instantiatedProjectile = Instantiate(projectile,
-                                                           transform.position,
-                                                           transform.rotation)
-                as Rigidbody;
-
-            instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed));
-
+            shoot(); //on lance shoot()
         }
+    }
+    private void shoot()
+    {
+        
+            if (sr.flipX == true) // On verifie si le SpriteRenderer de l'object qu'on a renseigner avant a flip sur x si oui: 
+            {
+                GameObject go = Instantiate(bulletPrefab, shootingPoint.position, transform.rotation); //on spawn un projectile au shooting.position et tourne comme le transform.rotation 
+                go.GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, 0); //On fais maintenant bouger l'object par rapport a la speed qu'on a mis avant 
+            }
+            else
+            {
+                GameObject go = Instantiate(bulletPrefab, shootingPoint.position, transform.rotation); // Meme chose que l'autre
+                go.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0); //Meme chose mais va de l'autre sens
+            }
+        
     }
 }
