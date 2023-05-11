e0c1f9187bb853bad14740f93c006600f25b8074
Sevirakath <tomstef34@gmail.com>
2023-05-11 11:54:55 +0200

    asset

--- Assets/Script/testlightFlicker.cs
+++ Assets/Script/testlightFlicker.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class testlightFlicker : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
+ 
+        [SerializeField] float firstVariable;
+        [SerializeField] float secondVariable;
+        [SerializeField] float secondsBetweenFlickers;
 
-    // Update is called once per frame
-    void Update()
+
+    private void start ()
+
     {
-        
+        myLight = Getcomponent<Light2D>();
+        StartCoroutine(LightFlicker());
+    }
+    IEnumerator LightFlicker()
+    {
+        yield return new WaitForseconds(secondsBetweenFlickers);
+        myLight.pointLightOuterRadius = Random.Range(firstVariable, secondVariable);
+        StartCoroutine(LightFlicker));
     }
 }
+
