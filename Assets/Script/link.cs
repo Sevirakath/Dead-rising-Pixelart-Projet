d6996eea57eb9c03aeaeb48e2d9f0c2071114171
Sevirakath <tomstef34@gmail.com>
2023-05-11 16:25:47 +0200

    asset

---
+++ Assets/Script/link.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class link : MonoBehaviour {
+
+	public void itchio(){
+		Application.OpenURL ("https://itch.io/");
+	}
+	public void twitter(){
+		Application.OpenURL ("https://twitter.com");
+	}
+	public void ETPA(){
+		Application.OpenURL ("https://www.etpa.com/");
+	}
+
+  public void youtube(){
+		Application.OpenURL ("https://www.youtube.com/channel/UCNrHxCpxDqJNPCn80oBI9pQ");
+}
+}
