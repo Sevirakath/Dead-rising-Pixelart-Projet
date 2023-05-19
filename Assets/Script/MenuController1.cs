25516f513555490ee34a2cf7d10d69dcaef7b24e
Sevirakath <tomstef34@gmail.com>
2023-05-19 16:56:57 +0200

    asset

--- Assets/Script/MenuController1.cs
+++ Assets/Script/MenuController1.cs
@@ -10,7 +10,7 @@ public class MenuController1 : MonoBehaviour
     [Header("Volume Setting")]
     [SerializeField] private TMP_Text volumeTextValue = null;
     [SerializeField] private Slider volumeSlider = null;
-
+    [SerializeField] private float defaultVolume = 1.0f;
     [Header("Confirmation")]
     [SerializeField] private GameObject confirmationPrompt = null;
 
@@ -55,6 +55,17 @@ public class MenuController1 : MonoBehaviour
         volumeTextValue.text = volume.ToString("0.0");
         PlayerPrefs.SetFloat("masterVolume", volume);
         StartCoroutine(ConfirmationBox());
+    }
+
+    public void ResetButton(string MenuType)
+    {
+        if(MenuType== "Audio")
+        {
+            AudioListener.volume = defaultVolume;
+            volumeSlider.value = defaultVolume;
+            volumeTextValue.text = defaultVolume.ToString("0.0");
+            VolumeApply();
+        }
     }
 
     public IEnumerator ConfirmationBox()
