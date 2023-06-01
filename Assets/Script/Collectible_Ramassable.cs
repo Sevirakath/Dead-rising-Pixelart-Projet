66e66c6ad99fb0eeefff6a7638d4a6c1a6cb4933
Sevirakath <tomstef34@gmail.com>
2023-06-02 00:57:37 +0200

    asset

--- Assets/Script/Collectible_Ramassable.cs
+++ Assets/Script/Collectible_Ramassable.cs
@@ -1,39 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Collectible_Ramassable : MonoBehaviour
 {
     private GameObject playerRef;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        PickUpPotion();
-    }
-
     void PickUpPotion()
     {
         if (playerRef && Input.GetKeyDown(KeyCode.E))
         {
-            playerRef.GetComponentInChildren<Health>().pv += 10;
+            Health playerHealth = playerRef.GetComponent<Health>();
+            if (playerHealth != null)
+            {
+                playerHealth.pv += 10;
+            }
             Destroy(gameObject);
         }
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        playerRef = collision.gameObject;
+        if (collision.CompareTag("Player"))
+        {
+            playerRef = collision.gameObject;
+        }
     }
 
-    void OnTriggerExit2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        playerRef = null;
+        if (collision.CompareTag("Player") && collision.gameObject == playerRef)
+        {
+            playerRef = null;
+        }
     }
+
+    // Remove the Start and Update methods if they are not used for anything else in your script.
 }
--- Assets/Script/IAdeType4.cs
+++ Assets/Script/IAdeType4.cs
@@ -1,9 +1,3 @@
-//Ne fonctionne pas encore appeler les fonctions est compliquer
-//Melange les 2 scripts
-
-
-
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,20 +11,16 @@ public class IAdeType4 : MonoBehaviour
     public Transform target;
     public float speedb = 5f;
     float distancemax = 10;
-    // Start is called before the first frame update
 
     bool atckType1 = true;
-    void Start()
-    {
-        
-    }
-    private void function1()
+
+    void function1()
     {
         transform.Translate(Vector2.right * speeda * Time.deltaTime);
         RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distancea);
-        if (groundInfo.collider == false)
+        if (!groundInfo.collider)
         {
-            if (movingRight == true)
+            if (movingRight)
             {
                 transform.eulerAngles = new Vector3(0, -180, 0);
                 movingRight = false;
@@ -43,33 +33,24 @@ public class IAdeType4 : MonoBehaviour
         }
     }
 
-
-    private void function2()
+    void function2()
     {
-
         transform.position = Vector3.MoveTowards(transform.position, target.position, speedb * Time.deltaTime);
         if (transform.position.y == 3)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         }
     }
-    // Start is called before the first frame update
 
-    // Update is called once per frame
-
-
-    // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(transform.position, target.transform.position) < distancemax)
-        
+        if (Vector3.Distance(transform.position, target.transform.position) < distancemax)
         {
             atckType1 = false;
-            //if(Input.GetKeyDown(KeyCode.P))
         }
         else
         {
-            atckType1 = true; 
+            atckType1 = true;
             if (atckType1)
             {
                 function1();
@@ -79,13 +60,5 @@ public class IAdeType4 : MonoBehaviour
                 function2();
             }
         }
-        
-       
     }
-    
-    // Start is called before the first frame update
-    
-
-    // Update is called once per frame
-  
 }
